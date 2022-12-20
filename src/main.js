648cd3b2fd14e5385025eb2c6f4ba80d8d54fd2c
appert <scoreur@gmail.com>
2022-12-20 10:19:28 +0100

    reprise projet

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import App from './App.vue'
+import router from './router'
+
+import './assets/tailwind.css'
+// https://www.npmjs.com/package/vue-material-design-icons
+
+
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+
+app.mount('#app')
